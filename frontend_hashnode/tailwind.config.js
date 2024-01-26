78808979fcadbfe586cef7c233644e6e5c5f7d1a
gateremark <mark5gatere@gmail.com>
2024-01-26 23:10:46 +0300

    refactor: edit tailwind config

--- frontend_hashnode/tailwind.config.js
+++ frontend_hashnode/tailwind.config.js
@@ -10,31 +10,6 @@ export default {
                 "inside-page":
                     "inset 20rem 0px 5rem -2.5rem rgba(0, 0, 0, 0.25)",
             },
-
-            backfaceVisibility: {
-                hidden: "hidden",
-            },
-            transformOrigin: {
-                left: "left",
-            },
-            transitionProperty: {
-                all: "all",
-            },
-            transitionDuration: {
-                1000: "1000ms",
-            },
-            transitionTimingFunction: {
-                ease: "ease",
-            },
-            rotate: {
-                "-180": "-180deg",
-            },
-            transformStyle: {
-                "preserve-3d": "preserve-3d",
-            },
-            perspective: {
-                100: "100rem",
-            },
         },
     },
     plugins: [],
